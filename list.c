83a3be233c57824567ef3fef778955d04dd4426f
Vadim Kindl <gildor.i@seznam.cz>
2004-04-19 08:32:42 +0000

    Added functions for adding element to first position and deleting element with any index. Corrected function for erasing first element.

--- list.c
+++ list.c
@@ -1,107 +1,102 @@
 #include "list.h"
 
 
-
 sList *CreateList()
-
 {
-
 	sList *list = (sList *)malloc(sizeof(sList));
 
-	
-
 	list->itemCount = 0;
-
 	list->first = NULL;
-
 	list->last = NULL;
 
-	
-
 	return list;
-
 }
 
 
 
 void DestroyList(sList *list)
-
 {
-
 	free(list);
-
 }
 
-
-
-void PushBack(sList *list, void *data)
-
+void PushFront(sList *list, void *data)
 {
-
 	sElement *element;
 
-	
-
-	if (list == 0)
-
-		return;
-
-		
+	if (!list) return;
 
 	element = (sElement *)malloc(sizeof(sElement));
-
 	element->data = data;
+	element->next = (void *)list->first;
 
-	element->next = NULL;
+	list->first = element;
 
-	
-
-	if (list->last)
-
-		list->last->next = (void *)element;
-
-	else
-
-		list->first = element;
-
-		
-
-	list->last = element;
+	if (!list->last)
+		list->last = (void *)element;
 
 	list->itemCount++;
-
 }
 
-
-
-void EraseFirst(sList *list)
-
+void PushBack(sList *list, void *data)
 {
-
 	sElement *element;
 
-	
+	if (!list) return;
 
-	if (list == 0)
+	element = (sElement *)malloc(sizeof(sElement));
+	element->data = data;
+	element->next = NULL;
 
-		return;
+	if (list->last)
+		list->last->next = (void *)element;
+	else
+		list->first = element;
 
-		
+	list->last = element;
+	list->itemCount++;
+}
+
+void EraseFront(sList *list)
+{
+	sElement *element;
+
+	if (!list) return;
 
 	element = list->first;
 
-		
-
 	if (list->first->next)
-
+	{
 		list->first = (void *)list->first->next;
-
-		
+	}
+	else
+	{
+		list->first = NULL;
+		list->last  = NULL;
+	}
 
 	list->itemCount--;
 
-	
-
 	free(element);
+}
 
+void EraseIndex(sList *list, int index)
+{
+	sElement *element;
+	void *previousElement;
+	int i;
+
+	if (!list) return;
+	if (list->itemCount > index) return;
+
+	element = list->first;
+
+	for (i = 0; i < index - 1; i ++)
+		element = (sElement *)element->next;
+
+	previousElement = element->next;
+	element->next = (void *)((sElement *)element->next)->next;
+
+	list->itemCount--;
+
+	free(previousElement);
 }
--- list.h
+++ list.h
@@ -1,52 +1,32 @@
 #ifndef _LIST_H_
-
 #define _LIST_H_
 
 
-
 #include <memory.h>
-
 #include <stdio.h>
 
 
-
 typedef struct
-
 {
-
 	struct sElement *next;
-
 	void *data;
-
 } sElement;
 
-
-
 typedef struct
-
 {
-
 	sElement *first;
-
 	sElement *last;
-
 	short itemCount;
-
 } sList;
 
 
-
 sList *CreateList();
-
 void DestroyList(sList *list);
 
-
-
+void PushFront(sList *list, void *data);
 void PushBack(sList *list, void *data);
-
-void EraseFirst(sList *list);
-
-
+void EraseFront(sList *list);
+void EraseIndex(sList *list, int index);
 
 #endif
 
