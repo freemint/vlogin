b34964397e744c27e2fbdd4308d916876f91a992
Vadim Kindl <gildor.i@seznam.cz>
2004-05-13 10:12:38 +0000

    Minor fixes.

--- debug.h
+++ debug.h
@@ -1,6 +1,10 @@
+#ifndef __DEBUG_H__
+#define __DEBUG_H__
 
 #define DEBUG debug_print
 void debug_print(char *s, ...);
 
 void InitDebug(void);
 void ExitDebug(void);
+
+#endif __DEBUG_H__
--- types.h
+++ types.h
@@ -1,35 +1,19 @@
 #ifndef _TYPES_H_
-
 #define _TYPES_H_
 
 
-
 typedef struct
-
 {
-
 	int	x;
-
 	int	y;
-
 } sPoint;
 
-
-
 typedef struct
-
 {
-
 	int	x;
-
 	int	y;
-
 	int	w;
-
 	int	h;
-
 } sRect;
-
-
 
 #endif _TYPES_H_
